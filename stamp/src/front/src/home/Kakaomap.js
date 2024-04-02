2b41d9aca4b70df26c6a1b43ae2b9985f5fd4964
skal48 <skal48@naver.com>
2024-04-03 03:33:17 +0900

    카카오 지도 페이지 추가

---
+++ stamp/src/front/src/home/Kakaomap.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+const Kakaomap = () =>{
+  const Key = process.env.REACT_APP_KAKAO_MAP_API_KEY;
+  return (
+      <div id="map" className={styles.map}>
+
+
+
+      </div>
+}
+
+export default Kakaomap
