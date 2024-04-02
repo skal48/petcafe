1412b5fd6269f140e6ab574c49e0c7ee313e55d7
skal48 <skal48@naver.com>
2024-04-03 04:12:28 +0900

    카카오 지도 표시

--- stamp/src/front/src/home/Home.js
+++ stamp/src/front/src/home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Kakaomap from "./Kakaomap";
 
 
 const Home = () => {
   return (
       <div style={{height:1000}}>
-
+        <Kakaomap />
       </div>
   )
 }
--- stamp/src/front/src/home/Kakaomap.js
+++ stamp/src/front/src/home/Kakaomap.js
@@ -1,14 +1,23 @@
-import React from "react";
-import axios from "axios";
+import React, {useEffect} from "react";
+import styles from './Kakaomap.module.scss';
+import { Map } from 'react-kakao-maps-sdk';
+
 
 const Kakaomap = () =>{
-  const Key = process.env.REACT_APP_KAKAO_MAP_API_KEY;
+
   return (
-      <div id="map" className={styles.map}>
-
-
-
-      </div>
+      <div className={styles.kmaps}>
+        <Map
+            center={{ lat: 33.5563, lng: 126.79581 }}   // 지도의 중심 좌표
+            style={{ width: '800px', height: '600px' }} // 지도 크기
+            level={3}                                   // 지도 확대 레벨
+        >
+        </Map>
+        
+        
+        요를ㄹ레이
+      </div>);
 }
+
 
 export default Kakaomap
