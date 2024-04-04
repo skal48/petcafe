1e1ad068d6811fb9bcbbb6f2e4a2bf39474008e4
skal48 <skal48@naver.com>
2024-04-05 00:14:47 +0900

    ResponseDto 생성

--- stamp/src/main/java/com/puppycafe/stamp/dto/ResponseDto.java
+++ stamp/src/main/java/com/puppycafe/stamp/dto/ResponseDto.java
@@ -7,7 +7,30 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(staticName = "set")
 @Builder
-public class ResponseDto {
+public class ResponseDto<D> {
+
+  //ResponseDto 만드는 이유 : API의 요청에 대한 응답을 간편하게 생성하고 전송할 수 있다.
+  private boolean result;
+  private String message;
+  private D data;
+
+  public static <D> ResponseDto<D> setSuccess(String message) {
+    return ResponseDto.set(true, message, null);
+  }
+
+  public static <D> ResponseDto<D> setFailed(String message) {
+    return ResponseDto.set(false, message, null);
+  }
+
+  public static <D> ResponseDto<D> setSuccessData(String message, D data) {
+    return ResponseDto.set(true, message, data);
+  }
+
+  public static <D> ResponseDto<D> setFailedData(String message, D data) {
+    return ResponseDto.set(false, message, data);
+  }
+
+
 }
