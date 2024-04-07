05f2c29a286429522e1d777a6d80bc8f4ebfd7ec
skal48 <skal48@naver.com>
2024-04-08 01:44:42 +0900

    mvcConfigurer 생성

--- stamp/src/main/java/com/puppycafe/stamp/controller/CafeController.java
+++ stamp/src/main/java/com/puppycafe/stamp/controller/CafeController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @AllArgsConstructor
 public class CafeController {
