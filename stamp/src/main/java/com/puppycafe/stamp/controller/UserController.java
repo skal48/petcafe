4a5b1131428efcd3d3aab0b2598b27e36e859f15
skal48 <skal48@naver.com>
2024-04-09 00:14:35 +0900

    UserDto rename/modify

--- stamp/src/main/java/com/puppycafe/stamp/controller/UserController.java
+++ stamp/src/main/java/com/puppycafe/stamp/controller/UserController.java
@@ -1,20 +1,16 @@
 package com.puppycafe.stamp.controller;
 
 import com.puppycafe.stamp.dto.ResponseDto;
-import com.puppycafe.stamp.dto.SignUpDto;
-import com.puppycafe.stamp.entity.User;
-import com.puppycafe.stamp.service.TestService;
+import com.puppycafe.stamp.dto.UserDto;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/auth")
 public class UserController {
     @PostMapping("/signUp")
-    public ResponseDto<SignUpDto> signUp(@RequestBody SignUpDto requestBody){
+    public ResponseDto<UserDto> signUp(@RequestBody UserDto requestBody){
         System.out.println(requestBody.toString());
         return null;
     }
--- stamp/src/main/java/com/puppycafe/stamp/dto/UserDto.java
+++ stamp/src/main/java/com/puppycafe/stamp/dto/UserDto.java
@@ -9,13 +9,16 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class SignUpDto {
+public class UserDto {
   private int userNo;
   private String userName;
   private String nickName;
   private String email;
   private String password;
   private int userType;
+  private String token;
+  private String userPhone;
+  private String businessRegistration;
   private LocalDate joinedDate;
   private int agree;
 }
