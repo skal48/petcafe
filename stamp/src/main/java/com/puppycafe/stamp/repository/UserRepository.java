73a961c6cd7f7ad2bac9e8bab1e0e91bffb1a2a4
skal48 <skal48@naver.com>
2024-04-09 02:19:25 +0900

    existsByEmail 추가

--- stamp/src/main/java/com/puppycafe/stamp/repository/UserRepository.java
+++ stamp/src/main/java/com/puppycafe/stamp/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.puppycafe.stamp.repository;
 
 import com.puppycafe.stamp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  @Query("SELECT u.email FROM User u WHERE u.email = :email")
+  boolean existsByEmail(@Param("email") String email);
 }
